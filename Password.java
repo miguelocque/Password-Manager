c76daa610a66f79c04ae826a5b8780f6fe8aaf3c
Miguel Ocque <mocque@bu.edu>
2025-05-25 16:52:26 -0400

    Made changes to the Password constructor and made code more readable. Found a potential edge case in the 'insert method' for which I have yet to write if statements for.

--- Password.java
+++ Password.java
@@ -15,14 +15,24 @@
 import java.util.*;
 import java.util.Random;
 
+
+
 public class Password {
+
+    // **************************** CORRECT BELOW THESE LINES *******************************
+
+    //  a global variable that initializes our hash table to fit 50 different accounts
+    private final int DEFAULT_NUM_PASSWORDS = 50;
+
     // a Node class that will contain all the fields of a Linked Password Node
     private class Node {
-        private Object username;
+        private String account_type;
+        private String username;
         private LLQueue<Object> PassValues;
         private Node next;
         
-        private Node(Object usr, Object pswrd) {
+        private Node(String acct, String usr, Object pswrd) {
+            this.account_type = acct;
             this.username = usr;
             PassValues = new LLQueue<Object>();
             PassValues.insert(pswrd);
@@ -39,61 +49,64 @@ public class Password {
         return h1;
     }
     
-    private Node[] table;      // the hash table itself
-    private int numKeys;       // the total number of keys in the table
+    // hash table with a set size
+    private Node[] table = new Node[DEFAULT_NUM_PASSWORDS];
 
-    // Password needs a constructor: we only need a number so that we can initialize the table
-    public Password (int numPasswords) {
-        if (numPasswords <= 0) {
-            throw new IllegalArgumentException("Must have a number of total passwords greater than 0");
-        }
+    // total number of passwords and accounts we've added to the hash table; updates within methods
+    private int numKeys; 
 
-        // initializes the table to the size given
-        table = new Node[numPasswords];
+    // Password needs a constructor, set the numKeys to 0 since we haven't added anything
+    public Password () {
 
-        // the numkeys tells us how many passwords we have
+        // the numkeys tells us how many passwords we have (0 at construction)
         numKeys = 0;
     }
 
-    // default constructor, no size given, so we assume 0 
-    public Password() {
-        this(0);
-    }
+    // ** not going to include any other constructors **
 
-    // ** not going to include any other constructors because we either need a number or none so that we can throw an exception **
+    // **************************** CORRECT ABOVE THESE LINES ********************************
 
     // method that will be called whenever a new password wants to be inserted
-    public boolean insertPass(String usrnm, Object pswrd) {
+    public boolean insertPass(String acct, String usrnm, Object pswrd) {
         // first we get the hash value of the username
-        int position = h1(usrnm);
+        int position = h1(acct);
 
         // then we look in the position and see if there's anything there to begin with
         // we also want to check if the position is 'removed' - there used to be a password that
         // we chose to delete
         if (table[position] == null || (table[position].username == null && table[position].next == null)) {
             // we can simply point the position to the new node we create
-            Node newAccount = new Node((String)usrnm, pswrd);
+            Node newAccount = new Node(acct, usrnm, pswrd);
             table[position] = newAccount;
             numKeys++;
         }
         else { // otherwise, there's something at the position, we must look for duplicates
+
+            // so we make a traverse Node at the correct table position
             Node trav = table[position];
-            while (trav != null && !trav.username.equals(usrnm)) {
+
+            // traverse through the things at the position until either we reach the end or find a dupe
+            while (trav != null && !trav.username.equals(usrnm) && !trav.account_type.equals(acct)) {
                 trav = trav.next;
             }
 
-            // here if trav is null, there are no duplicates, so we can add the new 'account' to the front of the list
+            // here if trav is null, there are no duplicates, so we can add the new 'account' to the FRONT of the list
             if (trav == null) {
                 // make the Node
-                Node newAccount = new Node((String)usrnm, pswrd);
+                Node newAccount = new Node(acct, usrnm, pswrd);
+
                 // point the next field of the new Node to the previous head 
                 newAccount.next = table[position];
+                
                 // make the new account the new head
                 table[position] = newAccount;
 
                 // update numKeys
                 numKeys++;
             }
+
+            // ** CHECK IF THE ACCT EQUALS BUT NOT THE USERNAME AND VICE VERSA
+
             else {  // otherwise, there is a duplicate, and trav is pointing to it
                 trav.PassValues.insert(pswrd);
 
@@ -106,9 +119,9 @@ public class Password {
     }
 
     // delete method
-    public boolean deletePass(Object usrnm, Object pswrdToDelete) {
+    public boolean deletePass(String acct, String usrnm, Object pswrdToDelete) {
         // get the hash value of the username
-        int position = h1(usrnm);
+        int position = h1(acct);
 
         // make a trav Node so that we can go through the linked list
         Node trav = table[position];
